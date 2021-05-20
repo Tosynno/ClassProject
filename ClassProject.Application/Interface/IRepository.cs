4e4af9372fd0d882bb5af772b6ad0ed6197af2e8
Tosin Raphael <tosynnogroup@outlook.com>
2021-05-20 15:21:36 +0100

    Add project files.

---
+++ ClassProject.Application/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using ClassProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject.Application.Interface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(int Id);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ ClassProject.Application/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using ClassProject.Application.Interface;
+using ClassProject.Domain.Entities;
+using ClassProject.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProject.Application.Repository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
